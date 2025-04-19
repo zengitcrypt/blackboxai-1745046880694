6ba313e7dd00cc74e7157751989fc4a5eae1c48b
zengitcrypt <zencrypto2000@gmail.com>
2025-04-19 15:14:47 +0800

    Add walletConfig.js 1745046882601

---
+++ walletConfig.js
@@ -0,0 +1,20 @@
+// Configuration for supported wallets
+const walletConfig = {
+  wallets: [
+    {
+      id: "metamask",
+      displayName: "MetaMask",
+      iconUrl: "https://images.pexels.com/photos/51994/wallet-cryptocurrency-coin-currency-51994.jpeg?auto=compress&cs=tinysrgb&w=600"
+    },
+    {
+      id: "trustwallet",
+      displayName: "Trust Wallet",
+      iconUrl: "https://images.pexels.com/photos/1454807/pexels-photo-1454807.jpeg?auto=compress&cs=tinysrgb&w=600"
+    },
+    {
+      id: "telegram",
+      displayName: "Telegram Wallet",
+      iconUrl: "https://upload.wikimedia.org/wikipedia/commons/8/82/Telegram_logo.svg"
+    }
+  ]
+};
